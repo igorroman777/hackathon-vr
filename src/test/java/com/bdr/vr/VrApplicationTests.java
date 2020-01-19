87481c58a3c2c7088f62484f4a7ba5b02f8e4372
p00920060 <igor.roman@bdr.de>
2020-01-19 18:54:29 +0100

    1. commit: initial commit

---
+++ src/test/java/com/bdr/vr/VrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdr.vr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
